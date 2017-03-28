377ab887ea7031a27fad43f9817ee2d6a33f52f3
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-28 23:20:16 +0000

    simplifying return statement

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, abort
+from flask import Flask
 app = Flask(__name__)
 
 
@@ -43,11 +43,7 @@ def number_route(n):
     """
     prints if n is an int
     """
-    try:
-        n = int(n)
-        return ("{} is a number".format(n))
-    except:
-        abort(404)
+    return "{} is a number".format(n)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)

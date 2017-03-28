945a0c0f08e94d3266b93e718cdd4a379d425599
davidknoppers <david.knoppers@holbertonschool.com>
2017-03-28 18:26:12 +0000

    first drafts of flask pys

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_hbnb():
+    return ('Hello HBNB!')
+@app.route('/hbnb')
+def just_hbnb():
+    return ('HBNB')
+@app.route('/c/<text>')
+def c_route(text):
+    return ("C "+text.replace("_"," "))
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_route(text="is cool"):
+    return ("Python "+text.replace("_"," "))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
